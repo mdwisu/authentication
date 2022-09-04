9886c8905f25693651d55ee267d0031412d2fe2e
mdwisu <dwisusanto784@gmail.com>
2022-09-04 21:02:21 +0700

    menambahkan fungsi getMe

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -27,6 +27,22 @@ export const LoginUser = createAsyncThunk(
   }
 );
 
+export const getMe = createAsyncThunk('user/getMe', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get('http://localhost:5000/me');
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      const message = error.response.data.msg;
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+});
+
+export const Logout = createAsyncThunk('user/Logout', async () => {
+  await axios.delete('http://localhost:5000/logout');
+});
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -47,6 +63,21 @@ export const authSlice = createSlice({
       state.isError = true;
       state.message = action.payload;
     });
+
+    // Get User Login
+    builder.addCase(getMe.pending, (state) => {
+        state.isLoading = true;
+      });
+      builder.addCase(getMe.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      });
+      builder.addCase(getMe.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
   },
 });
 
